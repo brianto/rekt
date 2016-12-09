cf15dd2ba616d3759f6245834985b61cf8106265
Brian To <bxt5647@rit.edu>
2016-12-08 22:52:47 -0800

    Add handlebars to compile step

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
 const gulp = require('gulp');
-
 const del = require('del');
+
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+
+const BUILD_DIR = 'build';
+
+gulp.task('clean', function() {
+  return del([ 'build' ]);
+});
+
+gulp.task('build', function() {
+  return gulp
+  .src('app/*.hbs')
+  .pipe(handlebars())
+  .pipe(rename(function(path) {
+    path.extname = '';
+  }))
+  .pipe(gulp.dest(BUILD_DIR))
+  ;
+});
+
+gulp.task('default', [ 'build' ]);
