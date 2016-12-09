20269d3b2604c647e5c8d7b964aff408eb2ffceb
Brian To <bxt5647@rit.edu>
2016-12-08 23:37:43 -0800

    Add bower js dependency compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 const gulp = require('gulp');
 const del = require('del');
+const path = require('path');
 
+const bower = require('bower-files')();
+const concat = require('gulp-concat');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 const BUILD_DIR = 'build';
 
 gulp.task('clean', function() {
   return del([ 'build' ]);
 });
+
+gulp.task('bower:js', function() {
+  return gulp
+  .src(bower.ext('js').files)
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(concat('vendor.min.js'))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest(path.join(BUILD_DIR, 'js')))
+  ;
+});
+
+gulp.task('bower', [
+  'bower:js',
+]);
 
 gulp.task('build', function() {
   return gulp
@@ -21,4 +41,4 @@ gulp.task('build', function() {
   ;
 });
 
-gulp.task('default', [ 'build' ]);
+gulp.task('default', [ 'bower', 'build' ]);
